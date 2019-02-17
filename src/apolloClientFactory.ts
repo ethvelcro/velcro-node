e272cc66a252eb55cfe335216ddbf1d3bd8664d6
Brendan Asselstine <mail.asselstine@gmail.com>
2019-02-16 23:18:16 -0700

    Fixed setup.ts

--- src/apolloClientFactory.ts
+++ src/apolloClientFactory.ts
@@ -1,16 +1,20 @@
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { WebSocketLink } from 'apollo-link-ws';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 
 export function apolloClientFactory(uri: string) {
-  const wsLink = new WebSocketLink({
+  const link = new WebSocketLink({
     uri,
     options: {
       reconnect: true
     }
   });
 
+  const cache = new InMemoryCache()
+
   return new ApolloClient({
-    link
+    link,
+    cache
   });
 }
